b161e3430281078a95df22994baada66d673649a
Miroslav Klimos <myreggg@gmail.com>
2013-01-31 18:53:28 +0000

    Rush hour update map - not yet working..

--- Sources/Client.WindowsStoreApp/pages/puzzleInstance/puzzleInstance.js
+++ Sources/Client.WindowsStoreApp/pages/puzzleInstance/puzzleInstance.js
@@ -6,66 +6,73 @@
     var controller;
 
     var Controller = WinJS.Class.define(
-        function(root) {
+        function (root) {
             this.rootElement = root;
             this.clockElement = document.getElementById("clock");
-            document.getElementById("continueButton").onclick = function(evt) {
+            document.getElementById("continueButton").onclick = function (evt) {
                 WinJS.Navigation.navigate("pages/home/home.html");
             };
         },
-        {
-            loadPuzzle: function (puzzle){
-                this.puzzle = new Puzzle(puzzle, this);
-                this.puzzle.initializeUI(this.rootElement.querySelector("section[role=main] .map"));
-                this.solved = false;
-                this.startClock();
-            },
+    {
+        loadPuzzle: function (puzzle) {
+            this.puzzle = new Puzzle(puzzle, this);
+            this.puzzle.initializeUI(this.rootElement.querySelector("section[role=main] .map"));
+            this.solved = false;
+            this.startClock();
+        },
 
-            startClock: function () {
-                this.startTime = Date.now();
-                this.refreshClock();
-            },
+        startClock: function () {
+            this.startTime = Date.now();
+            this.refreshClock();
+        },
 
-            refreshClock: function () {
-                if (!this.solved) {
-                    this.clockElement.textContent = this.formatTimeDiff(Date.now() - this.startTime);
-                    window.setTimeout(this.refreshClock.bind(this), 1000);
-                }
-            },
-
-            formatTimeDiff: function(timediff) {
-                var seconds = Math.floor(timediff / 1000);
-                var minutes = Math.floor(seconds / 60);
-                seconds = seconds % 60;
-                return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-            },
-
-            action_solved: function () {
-                if (this.solved) return;
-                this.solved = true;
-                this.endTime = Date.now();
-                //document.getElementById("solvingTime").textContent = this.formatTimeDiff(this.endTime - this.startTime);
-                var dialog = new Windows.UI.Popups.MessageDialog("Congratulations! Puzzle solved in " + this.formatTimeDiff(this.endTime - this.startTime), "Finished!");
-                dialog.commands.append(new Windows.UI.Popups.UICommand("Continue", function() {
-                    WinJS.Navigation.navigate("pages/home/home.html");
-                }));
-                dialog.showAsync().done(function() {
-                });
-                
-                //a.operation.start();
-                //document.getElementById("solvedFlyout").winControl.show(
-                //    this.rootElement, "top");
+        refreshClock: function () {
+            if (!this.solved) {
+                this.clockElement.textContent = this.formatTimeDiff(Date.now() - this.startTime);
+                window.setTimeout(this.refreshClock.bind(this), 1000);
             }
-        }, {
-           // static elements 
-        });
+        },
+
+        formatTimeDiff: function (timediff) {
+            var seconds = Math.floor(timediff / 1000);
+            var minutes = Math.floor(seconds / 60);
+            seconds = seconds % 60;
+            return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+        },
+
+        action: function (obj) {
+            console.log(JSON.stringify(obj));
+        },
+
+        action_solved: function () {
+            if (this.solved) return;
+            this.solved = true;
+            this.endTime = Date.now();
+            //document.getElementById("solvingTime").textContent = this.formatTimeDiff(this.endTime - this.startTime);
+            var dialog = new Windows.UI.Popups.MessageDialog("Congratulations! Puzzle solved in " + this.formatTimeDiff(this.endTime - this.startTime), "Finished!");
+            dialog.commands.append(new Windows.UI.Popups.UICommand("Continue", function () {
+                WinJS.Navigation.navigate("pages/home/home.html");
+            }));
+            dialog.commands.append(new Windows.UI.Popups.UICommand("Continue with challenge", function () {
+                WinJS.Navigation.navigate("pages/home/home.html");
+            }));
+            dialog.showAsync().done(function () {
+            });
+
+            //a.operation.start();
+            //document.getElementById("solvedFlyout").winControl.show(
+            //    this.rootElement, "top");
+        }
+    }, {
+        // static elements 
+    });
 
 
 
     ui.Pages.define("/pages/puzzleInstance/puzzleInstance.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
-        ready: function(element, options) {
+        ready: function (element, options) {
             this.controller = new Controller(element);
             this.rootElement = element;
             this.loaded = { "js": false, "css": false, "puzzle": false };
@@ -74,7 +81,7 @@
                 this.loaded.puzzle = true;
             } else if (options.puzzleMeta) {
                 this.puzzle = options.puzzleMeta;
-                DAL.whenPuzzleLoaded(this.puzzle.id).then(function(puzzle) {
+                DAL.whenPuzzleLoaded(this.puzzle.id).then(function (puzzle) {
                     this.puzzle = puzzle;
                     this.loadedEvent("puzzle");
                 }.bind(this));
@@ -87,7 +94,7 @@
         loadJS: function (puzzleType) {
             var script = document.createElement("script");
             this.rootElement.appendChild(script);
-            script.onload = function() { this.loadedEvent("js"); }.bind(this);
+            script.onload = function () { this.loadedEvent("js"); }.bind(this);
             script.setAttribute("src", "puzzles/" + puzzleType + "/script.js");
         },
 
@@ -100,7 +107,7 @@
             stylesheet.setAttribute("href", 'puzzles/' + puzzleType + '/style.css');
         },
 
-        loadedEvent: function(what) {
+        loadedEvent: function (what) {
             this.loaded[what] = true;
             if (this.loaded["js"] == true && this.loaded["css"] == true && this.loaded["puzzle"] == true) {
                 this.controller.loadPuzzle(this.puzzle);
@@ -108,11 +115,11 @@
         },
 
         // This function updates the page layout in response to viewState changes.
-        updateLayout: function(element, viewState, lastViewState) {
+        updateLayout: function (element, viewState, lastViewState) {
         },
-        
+
     });
-   
+
 })();
 
-  
+
